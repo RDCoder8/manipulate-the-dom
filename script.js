58b231c78d91c8fc78477931f7523223a22f87c0
RDCoder8 <mojinr@gmail.com>
2023-07-23 23:42:11 -0400

    Assignment Complete

--- script.js
+++ script.js
@@ -17,32 +17,66 @@ document.addEventListener("DOMContentLoaded", function(event) {
   // Do all of your work inside the document.addEventListener  
 
   // Part 1
-
+const mainTitle = document.getElementById('main-title')
+mainTitle.textContent = "Welcome to the House of DOM"
 
   // Part 2
-
+const body = document.querySelector('body')
+body.style.backgroundColor = 'royalblue'
 
   // Part 3
+const favoriteThings = document.getElementById('favorite-things')
+favoriteThings.removeChild(favoriteThings.children[favoriteThings.children.length - 1])
 
 
   // Part 4
-
+const specialTitles = document.querySelectorAll('.special-title')
+specialTitles.forEach(element => {
+  element.style.fontSize = '2rem'
+})
 
   // Part 5
-
+const pastRaces = document.getElementById('past-races')
+//pastRaces.removeChild(pastRaces.children[3]) //The Easy Way
+for (let i = 0; i < pastRaces.children.length; i++) { //Gotten programmatically
+  const element = pastRaces.children[i];
+  if (element.textContent === 'Chicago') {
+    element.remove()
+  }
+}
 
   // Part 6
-
+const newCity = document.createElement('i')
+newCity.textContent = "Atlanta"
+pastRaces.append(newCity)
 
   // Part 7
+const newBlogPost = document.createElement('div')
+const newBlogPostLocation = document.createElement('h1')
+const newBlogPostSubText = document.createElement('p')
+const main = document.querySelector('.main')
 
+newBlogPost.classList.add('blog-post', 'purple')
+newBlogPostLocation.textContent = 'Atlanta'
+newBlogPostSubText.textContent = 'I crashed through the georgia aquarium!'.toUpperCase()
+
+newBlogPost.append(newBlogPostLocation, newBlogPostSubText)
+main.append(newBlogPost)
 
   // Part 8
-
+const randomQuoteBtn = document.getElementById('quote-title')
+randomQuoteBtn.addEventListener('click', randomQuote)//Note to self, don't need parentheses inside.
 
   // Part 9
-
-
+const blogPosts = document.querySelectorAll('.blog-post')
+blogPosts.forEach(element => {
+  element.addEventListener('mouseout', (evt) => {
+    evt.target.classList.toggle('purple') //This definitely works differently than hover.
+  })
+  element.addEventListener('mouseenter', (evt) => {
+    evt.target.classList.toggle('red') //Yeah, I think I'll stick to hover for this.
+  })
+})
 
 
 });
